0f50d147dfdedd25f78963c0f236b67123e3e139
jiny3360 <jiny3360@naver.com>
2024-05-17 14:30:15 +0900

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main>
+      Sidebar
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
